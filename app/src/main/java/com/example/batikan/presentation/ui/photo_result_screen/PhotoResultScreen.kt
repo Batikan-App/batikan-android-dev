ac90b6d136fd1589c5ffe1580e7729671f5f6e9f
prammmoe <ikhwanpramuditha05@gmail.com>
2024-11-23 12:53:29 +0700

    feat: finish slicing photo result screen

--- app/src/main/java/com/example/batikan/presentation/ui/photo_result_screen/PhotoResultScreen.kt
+++ app/src/main/java/com/example/batikan/presentation/ui/photo_result_screen/PhotoResultScreen.kt
@@ -1,23 +1,35 @@
 package com.example.batikan.presentation.ui.photo_result_screen
 
 import android.net.Uri
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
+import com.example.batikan.presentation.ui.composables.ButtonUi
+import com.example.batikan.presentation.ui.theme.Primary600
+import com.example.batikan.presentation.ui.theme.White
 import java.io.File
 
 @Composable
@@ -35,19 +47,45 @@ fun PhotoResultScreen(navController: NavController, photoUri: String?) {
                 contentDescription = "Hasil Foto",
                 modifier = Modifier
                     .align(Alignment.Center)
-                    .size(300.dp)
+                    .fillMaxSize()
             )
         }
 
-        Button(
-            onClick = { navController.navigateUp() },
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(16.dp)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
         ) {
-            Text("Kembali ke Kamera")
+            Button(
+                onClick = { navController.navigateUp() },
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                    .size(width = 400.dp, height = 50.dp),
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Primary600, contentColor = White
+                )
+            ) {
+                Text("Lanjutkan")
+            }
+            OutlinedButton(
+                onClick = { navController.navigateUp() },
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                    .size(width = 400.dp, height = 50.dp),
+                shape = RectangleShape,
+                border = BorderStroke(1.dp, Primary600),
+                colors = ButtonDefaults.outlinedButtonColors(contentColor = Primary600),
+            ) {
+                Text("Foto Ulang")
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPhotoResultScreen() {
+    PhotoResultScreen(navController = rememberNavController(), photoUri = "https://images.unsplash.com/photo-1672716912554-c23ba8fac4ce?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmF0aWt8ZW58MHx8MHx8fDA%3D")
 }
 
 
