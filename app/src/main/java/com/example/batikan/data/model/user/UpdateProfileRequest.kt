0d31815771ab298ccb88ec30d5f9c024dfef0537
prammmoe <ikhwanpramuditha05@gmail.com>
2024-12-06 09:36:21 +0700

    feat(hotfix): fix bug in login

---
+++ app/src/main/java/com/example/batikan/data/model/user/UpdateProfileRequest.kt
@@ -0,0 +1,9 @@
+package com.example.batikan.data.model.user
+
+data class UpdateProfileRequest(
+    val name: String,
+    val email: String,
+    val phone: String,
+    val password: String,
+    val verifyPassword: String
+)
